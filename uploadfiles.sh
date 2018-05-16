7da9912cf4e8ff72c0bdbada95019abdbc1c4c8b
charliep127 <charlie@weathertogether.us>
2018-05-16 04:37:56 -0100

    Add files via upload
    Fixed bug that made charts with string in filename that was equivalent to other characters in filename (etc. 500mb for hour 00) upload multiple times by adding an if statement.

---
+++ uploadfiles.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#$1=model
+#$2=intdate
+#$3=inithour
+#$4=forecasthour
+
+#scp -i /home/mint/.ssh/id_rsa /home/mint/grads_pics/${1}/${2}/${3}z/*${4}hrfcst.png /home/mint/grads_pics/${1}/${2}/${3}z/*.txt weathfc0@weathertogether.net:public_html/models/${1}/${2}/${3}z/ 
+
+#scp -i /home/mint/.ssh/id_rsa /home/mint/grads_pics/${1}/${2}/${3}z/*.txt weathfc0@weathertogether.net:public_html/models/${1}/latest/${3}z/
+
+if [ "$1" == "HRRR_Sub" ] ; then 
+                scp -i /home/mint/.ssh/id_rsa /home/mint/grads_pics/${1}/${2}/${3}z/*${4}*.png /home/mint/grads_pics/${1}/${2}/${3}z/*.txt weathfc0@weathertogether.net:public_html/models/${1}/${2}/${3}z/ 
+            else
+                scp -i /home/mint/.ssh/id_rsa /home/mint/grads_pics/${1}/${2}/${3}z/*${4}.png /home/mint/grads_pics/${1}/${2}/${3}z/*.txt weathfc0@weathertogether.net:public_html/models/${1}/${2}/${3}z/ 
+            fi
+
+
+
+scp -i /home/mint/.ssh/id_rsa /home/mint/grads_pics/${1}/${2}/${3}z/*.txt weathfc0@weathertogether.net:public_html/models/${1}/latest/${3}z/
